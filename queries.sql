41340280f6e92ae83bc6a5b8a8e1fb5222b7e2d8
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-31 14:37:16 +0300

    fix max and min weight query

--- queries.sql
+++ queries.sql
@@ -31,6 +31,6 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts > 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, Max(escape_attempts) FROM animals GROUP BY neutered;
-SELECT species, MIN(weight_kg) FROM animals GROUP BY species;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals GROUP BY species, date_of_birth HAVING date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31';
 
