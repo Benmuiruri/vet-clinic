4f9d97e039be137d2fbcd082c6efa0c6c6650abe
Benmuiruri <ben.muiruri13@gmail.com>
2022-05-31 14:36:06 +0300

    fix max escaped attempts query

--- queries.sql
+++ queries.sql
@@ -30,7 +30,7 @@ COMMIT;
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts > 0;
 SELECT AVG(weight_kg) FROM animals;
-SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+SELECT neutered, Max(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals GROUP BY species, date_of_birth HAVING date_of_birth >= '1990-01-01' AND date_of_birth <= '2000-12-31';
 
