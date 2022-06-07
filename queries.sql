fa22de3644d4ff3196acbc3d7ef2d8cfa4121519
Benmuiruri <ben.muiruri13@gmail.com>
2022-06-07 12:15:01 +0300

    Update README.md and optimize DB

--- queries.sql
+++ queries.sql
@@ -183,3 +183,12 @@ GROUP BY species.name
 ORDER BY COUNT DESC
 LIMIT 1;
 
+/* Explain Analyze */
+
+explain analyze SELECT COUNT(*) FROM visits where animal_id = 4; 
+
+explain analyze SELECT * FROM visits where vet_id = 2;
+
+explain analyze SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+explain analyze SELECT * FROM visits where vet_id = 2 ORDER BY id DESC;
